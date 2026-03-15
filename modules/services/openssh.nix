b05846ff83d3be554538c4816a71eb6fee61f8db
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2026-03-15 14:26:25 +0100

    chore(machines/all): change SSH server port

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -1,7 +1,9 @@
+{ lib, ... }:
 {
   services.openssh.nixos.services = {
     openssh = {
       enable = true;
+      ports = lib.mkForce [ 2222 ];
       openFirewall = true;
       settings = {
         PermitRootLogin = "prohibit-password";
