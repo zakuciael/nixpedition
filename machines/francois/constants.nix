b2bff3ddd503c35d868c5f3fe630fc13af37b359
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2026-07-31 21:34:56 +0200

    feat(services/nixbot): add eval & build settings

--- machines/francois/constants.nix
+++ machines/francois/constants.nix
@@ -15,9 +15,24 @@
         nixbot = {
           port = 8010;
           domain = "ci.zakku.eu";
-          github = {
-            appId = 4430921;
-            oauthId = "Iv23liJZTjKA89uNvkRT";
+          settings = {
+            eval = {
+              worker_count = 6;
+              memory_limit = 3072;
+            };
+            build = {
+              concurrency = 6;
+              systems = [
+                "x86_64-linux"
+                "aarch64-linux"
+                "aarch64-darwin"
+              ];
+            };
+
+            github = {
+              appId = 4430921;
+              oauthId = "Iv23liJZTjKA89uNvkRT";
+            };
           };
         };
       };
